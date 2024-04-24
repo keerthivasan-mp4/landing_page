646b940cbfd824b9b86ef61ac72ce58cfbc8d0f2
keerthi vasan.J <keerthiv741@gmail.com>
2024-04-24 22:08:42 +0530

    first commit, would be changed later

---
+++ script.js
@@ -0,0 +1,21 @@
+function imgslider(img){
+    document.querySelector('.item').src = img;
+    
+}
+
+function changecirclecolor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
+
+// if(imgslider('#img2'))
+// {
+//     document.querySelector('#desc').textContent= 'wraps and role';
+// }
+
+// else if(imgslider('#img3'))
+// {
+
+//     document.querySelector('#desc').textContent= 'dessert and much more';
+// }
+
